fc43b88037125231c707e26d8eb64824ba40f34c
17138681A <william.tsang24@gmail.com>
2019-12-01 23:38:30 +0800

    second update

--- HealthPack.cpp
+++
@@ -1,9 +0,0 @@
-//#include "HealthPack.h"
-
-//HealthPack::HealthPack(double x, double y, QTimer* timer, QObject* parent):Equipment(x, y, timer, parent){
-
-//    name = HEALTH_PACK;
-//    pix = QPixmap(":/image/healthpack.png");
-//    setPixmap(pix);
-
-//}
